35618a025dacd5ddf12d3719627af2c4ac381c2c
Chris <c.lamothe25@gmail.com>
2018-03-06 13:59:34 -0500

    Data binding with v-bind and v-html

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue ({
   el: '#vue-app',
   data: {
     name: 'Chris',
-    job: 'Coding Ninja'
+    job: 'Coding Ninja',
+    website: 'http://www.thenetninja.co.uk',
+    websiteTag: "<a href='http://www.thenetninja.co.uk'>The Net Ninja Website</a>"
   },
   methods: {
     greet: function(time) {
